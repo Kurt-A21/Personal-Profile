11f7dc30f20815c8d71ed598a7e680183145322e
Kurt Adriaanse <kurt.adriaanse@clickatell.com>
2023-07-15 19:37:22 +0200

    Updated and fix navbar bugs

--- Script/topnav_functionality.js
+++ Script/topnav_functionality.js
@@ -1,8 +1,12 @@
-function navbar() {
-    var x = document.getElementById("myTopNav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    menu.classList.remove('show');
   }
+});
