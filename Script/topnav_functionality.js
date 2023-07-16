14264e9901d7cdaf18bd1666894c7c2a12b390a1
Kurt Adriaanse <kurt.adriaanse@clickatell.com>
2023-07-17 00:06:33 +0200

    Updated home page and navbar

--- Script/topnav_functionality.js
+++ Script/topnav_functionality.js
@@ -1,12 +1,21 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector(".menu");
 
-menuIcon.addEventListener('click', () => {
-  menu.classList.toggle('show');
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("show");
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
-    menu.classList.remove('show');
+    menu.classList.remove("show");
   }
 });
+
+//overlay
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
